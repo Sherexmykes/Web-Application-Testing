81f599fb2dc1c977404d8452bc0966d489291aa1
Sheila R Moore <sherexmykes@github.com>
2019-10-10 22:13:22 -0400

    mvp i think

---
+++ web-app-testing/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+import Display from './Display';
+
+function App() {
+  return (
+    <div className="App">
+      <Display/>
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
